ada482f480a3475db88a51f255f27288b8b0a1d1
yandex-lyceum-yakovlev <yakovlev@yandexlyceum.ru>
2022-02-01 16:07:51 +0400

    draw

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 import tkinter
 
+
+def draw(board):
+    for j in range(m):
+        for i in range(n):
+            c.create_rectangle(j * cell_height, i * cell_width, (j + 1) * cell_height, (i + 1) * cell_width,
+                               fill=board[i][j])
+
 window = tkinter.Tk()
 
 width = height = 600
@@ -9,8 +16,9 @@ cell_height = height // n
 c = tkinter.Canvas(window, width=width, height=height, bg='white')
 c.pack()
 
-for i in range(n):
-    for j in range(m):
-        c.create_rectangle(i * cell_height, j*cell_width, (i + 1) * cell_height, (j+1) * cell_width, fill="red")
+board = [["red"] * m for i in range(n)]
+board[2][5] = "green"
+board[3][2] = "blue"
+draw(board)
 
 window.mainloop()
